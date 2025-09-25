68f38473bb59a09051c9c6a4dc989de74773a32a
ibrahim <ibrahim.imran@iaim.ca>
2025-09-25 21:33:36 +0500

    trigger commit

--- .botpress/implementation/typings/actions/index.ts
+++ .botpress/implementation/typings/actions/index.ts
@@ -5,8 +5,11 @@ import * as getRate from "./getRate/index";
 export * as getRate from "./getRate/index";
 import * as convertAmount from "./convertAmount/index";
 export * as convertAmount from "./convertAmount/index";
+import * as parseAndConvert from "./parseAndConvert/index";
+export * as parseAndConvert from "./parseAndConvert/index";
 
 export type Actions = {
   "getRate": getRate.GetRate;
   "convertAmount": convertAmount.ConvertAmount;
+  "parseAndConvert": parseAndConvert.ParseAndConvert;
 }
